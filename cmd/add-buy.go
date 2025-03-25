a2ff0d1559d517af1719695c62af3541bf69adb4
Andreas Affentranger <andreas.affentranger@stud.hslu.ch>
2025-03-25 01:19:22 +0100

    refactor and testcases added for add-buy

--- cmd/add-buy.go
+++ cmd/add-buy.go
@@ -3,11 +3,21 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"github.com/ruegerj/stock-sight/internal/repository"
-	"github.com/spf13/cobra"
 	"strconv"
 	"time"
+
+	"github.com/ruegerj/stock-sight/internal/repository"
+	"github.com/spf13/cobra"
 )
+
+type BuyCmdParams struct {
+	Ticker             string
+	PricePerUnit       float64
+	Currency           string
+	Amount             float64
+	Date               time.Time
+	TransactionTypeBuy bool
+}
 
 func AddBuyCmd(ctx context.Context, transactionRepo repository.TransactionRepository) CobraCommand {
 	addBuyCmd := &cobra.Command{
@@ -15,55 +25,19 @@ func AddBuyCmd(ctx context.Context, transactionRepo repository.TransactionReposi
 		Short: "Adds a buy for a stock to your portfolio",
 		RunE: func(cmd *cobra.Command, args []string) error {
 
-			errorText := ""
-			ticker := cmd.Flag("stock").Value.String()
-			if len(ticker) < 3 {
-				errorText += "please enter a valid --stock \n"
-			}
-
-			ppu, err := strconv.ParseFloat(cmd.Flag("ppu").Value.String(), 64)
-			if err != nil || ppu < 0 {
-				errorText += "please enter a valid price per unit (--ppu) \n"
-			}
-
-			amount, err := strconv.ParseFloat(cmd.Flag("amount").Value.String(), 64)
-			if err != nil || amount < 0 {
-				errorText += "please enter a valid --amount \n"
-			}
-
-			currency := cmd.Flag("currency").Value.String()
-			if currency != "USD" && currency != "EUR" && currency != "CHF" {
-				errorText += "please enter a valid --currency (USD, EUR, CHF) \n"
-			}
-
-			dateStr := cmd.Flag("date").Value.String()
-			date, err := time.Parse("02.01.2006 15:04:05", dateStr)
-			if nil != err {
-				if dateStr == "now" {
-					date = time.Now()
-				} else {
-					errorText += "please enter a valid date (dont forget parenthesis): --date \"DD.MM.YYYY hh:mm:ss\" \n" + err.Error()
-				}
-			}
-
-			transactionStr := cmd.Flag("transaction").Value.String()
-			if transactionStr != "buy" && transactionStr != "sell" {
-				errorText += "please enter a valid transaction direction: --transaction (buy / sell) \n"
-			}
-			isBuy := transactionStr == "buy"
-
-			if errorText != "" {
-				fmt.Printf("Error: \n%s", errorText)
+			params, err := ParseBuyCmdFlags(cmd)
+			if err != nil {
+				fmt.Print(err.Error())
 				return nil
 			}
 
-			_, err = transactionRepo.Create(ctx, ticker, amount, currency, ppu, date, isBuy)
+			_, err = transactionRepo.Create(ctx, params.Ticker, params.Amount, params.Currency, params.PricePerUnit, params.Date, params.TransactionTypeBuy)
 			if err != nil {
 				return err
 			}
 
 			fmt.Printf("Added the following transaction to the Portfolio\n")
-			fmt.Printf("Ticker: {%s} |Ppu: {%f} {%s} |Amount: {%f} | Date: {%s} | Buy: {%s} \n", ticker, ppu, currency, amount, date.Format("02.01.2006 15:04:05"), transactionStr)
+			fmt.Printf("Ticker: {%s} |Ppu: {%f} {%s} |Amount: {%f} | Date: {%s} | Buy: {%t} \n", params.Ticker, params.PricePerUnit, params.Currency, params.Amount, params.Date.Format("02.01.2006 15:04:05"), params.TransactionTypeBuy)
 
 			return nil
 		},
@@ -80,4 +54,58 @@ func AddBuyCmd(ctx context.Context, transactionRepo repository.TransactionReposi
 		cmd:  addBuyCmd,
 		path: "root add-buy",
 	}
+}
+
+// TODO: further refactor later
+func ParseBuyCmdFlags(cmd *cobra.Command) (BuyCmdParams, error) {
+	errorText := ""
+	ticker := cmd.Flag("stock").Value.String()
+	if len(ticker) < 3 {
+		errorText += "please enter a valid --stock \n"
+	}
+
+	ppu, err := strconv.ParseFloat(cmd.Flag("ppu").Value.String(), 64)
+	if err != nil || ppu < 0 {
+		errorText += "please enter a valid price per unit (--ppu) \n"
+	}
+
+	amount, err := strconv.ParseFloat(cmd.Flag("amount").Value.String(), 64)
+	if err != nil || amount < 0 {
+		errorText += "please enter a valid --amount \n"
+	}
+
+	currency := cmd.Flag("currency").Value.String()
+	if currency != "USD" && currency != "EUR" && currency != "CHF" {
+		errorText += "please enter a valid --currency (USD, EUR, CHF) \n"
+	}
+
+	dateStr := cmd.Flag("date").Value.String()
+	date, err := time.Parse("02.01.2006 15:04:05", dateStr)
+	if nil != err {
+		if dateStr == "now" {
+			date = time.Now()
+		} else {
+			errorText += "please enter a valid date (dont forget parenthesis): --date \"DD.MM.YYYY hh:mm:ss\" \n" + err.Error()
+		}
+	}
+
+	transactionStr := cmd.Flag("transaction").Value.String()
+	if transactionStr != "buy" && transactionStr != "sell" {
+		errorText += "please enter a valid transaction direction: --transaction (buy / sell) \n"
+	}
+	isBuy := transactionStr == "buy"
+
+	if errorText != "" {
+		return BuyCmdParams{}, fmt.Errorf("error: \n%s", errorText)
+	}
+
+	return BuyCmdParams{
+		Ticker:             ticker,
+		PricePerUnit:       ppu,
+		Currency:           currency,
+		Amount:             amount,
+		Date:               date,
+		TransactionTypeBuy: isBuy,
+	}, nil
+
 }

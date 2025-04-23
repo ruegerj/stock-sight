0dc38d665b8b16f48cdbcd4546b22ba98c167f3e
Andreas Affentranger <andreas.affentranger@stud.hslu.ch>
2025-04-23 13:55:48 +0200

    add tui watchlist WIP

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -2,9 +2,11 @@ package tui
 
 import (
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/ruegerj/stock-sight/internal/watchlist"
 )
 
 type Model struct {
+	watchlist watchlist.Watchlist
 }
 
 func (m Model) Init() tea.Cmd {
@@ -13,6 +15,9 @@ func (m Model) Init() tea.Cmd {
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.watchlist.SetHeight(msg.Height)
+		m.watchlist.SetWidth(msg.Width)
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "q", "ctrl+c":
@@ -23,11 +28,24 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m Model) View() string {
-	return "Hello, from the TUI!"
+	return m.watchlist.View()
 }
 
 func Start() error {
-	p := tea.NewProgram(Model{}, tea.WithAltScreen())
+	entry := watchlist.WatchlistEntry{
+		Ticker:   "APPL",
+		Name:     "Apple Inc.",
+		Ppu:      32.0,
+		Currency: "USD",
+		Amount:   12,
+	}
+
+	m := Model{
+		watchlist: watchlist.New(),
+	}
+
+	m.watchlist.AddWatchlistEntry(entry)
+	p := tea.NewProgram(m, tea.WithAltScreen())
 	_, err := p.Run()
 	return err
 }
